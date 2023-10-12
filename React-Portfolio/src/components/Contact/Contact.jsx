d5af35d28cf6216ce90e98599aba0c4edbd18b78
Bemon <tristan2107b@gmail.com>
2023-10-12 19:34:28 +1100

    Added contact code and styling and netlify functionality

--- React-Portfolio/src/components/Contact/Contact.jsx
+++ React-Portfolio/src/components/Contact/Contact.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import './Contact.css';
 
 function Contact() {
-  return <div>Contact Page Placeholder</div>;
+    return (
+        <div className="contact-container">
+            <form className="contact-form" data-netlify="true" name="contact">
+                <input type="hidden" name="form-name" value="contact" />
+                <div className="input-group">
+                    <label>
+                        Name<span>*</span>
+                        <input type="text" placeholder="Name..." required />
+                    </label>
+                    <p className="input-error">Name is required!</p>
+                </div>
+                <div className="input-group">
+                    <label>
+                        Email<span>*</span>
+                        <input type="email" placeholder="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required />
+                    </label>
+                    <p className="input-error">Please enter a valid email address!</p>
+                </div>
+                <div className="input-group">
+                    <label>
+                        Message
+                        <textarea placeholder="message" required></textarea>
+                    </label>
+                    <p className="input-error">Message is required!</p>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default Contact;
